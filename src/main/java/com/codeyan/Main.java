c3adce68c39aabe233d54ef15b63683eaebee12f
Yan Avlis <ccyan05@alu.ufc.br>
2024-05-19 13:02:08 -0300

    System calls

--- src/main/java/com/codeyan/Main.java
+++ src/main/java/com/codeyan/Main.java
@@ -1,7 +1,21 @@
 package com.codeyan;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Makes a system call to list all files on ./
+        try {
+            var process = Runtime.getRuntime().exec("ls"); // Unix-like system command
+            var reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            process.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
