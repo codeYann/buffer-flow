600485f17b1df0e6579fadc81382d8dd2a93d384
Yan Avlis <ccyan05@alu.ufc.br>
2024-05-21 10:53:18 -0300

    Main

--- src/main/java/com/codeyan/Main.java
+++ src/main/java/com/codeyan/Main.java
@@ -5,23 +5,33 @@ import com.codeyan.Consumer.Consumer;
 import com.codeyan.Producer.Producer;
 
 import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.function.Supplier;
 
 public class Main {
+    private static int NUM_PRODUCERS = 2;
+    private static int NUM_CONSUMERS = 3;
+
+    private static Supplier<Integer> generateSupplier(Random random, int interval) {
+        Supplier<Integer> sup = () -> random.nextInt(interval);
+        return sup;
+    }
+
     public static void main(String[] args) {
-        Buffer<Integer> buffer = new Buffer<Integer>(3);
+
+        Buffer<Integer> buffer = new Buffer<Integer>(55);
 
         Random random = new Random();
+        var sup = generateSupplier(random, 25);
 
-        Supplier<Integer> supplier = () -> random.nextInt(25);
+        ExecutorService executor = Executors.newFixedThreadPool(NUM_PRODUCERS + NUM_CONSUMERS + 1);
 
-        Producer<Integer> producer = new Producer<Integer>(buffer, supplier);
-        Consumer consumer = new Consumer(buffer);
+        for (int i = 0; i < NUM_PRODUCERS; i++)
+            executor.execute(new Producer<Integer>(i, buffer, sup));
 
-        Thread thread = new Thread(producer);
-        Thread thread2 = new Thread(consumer);
-
-        thread.start();
-        thread2.start();
+        for (int i = 0; i < NUM_PRODUCERS; i++)
+            executor.execute(new Consumer(i, buffer));
     }
 }
